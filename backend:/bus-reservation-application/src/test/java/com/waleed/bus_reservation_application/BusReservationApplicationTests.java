87ed5807c56f2c6c0a0b9fca74dd13110309b180
Waleed Alaql <waleedalaql10@gmail.com>
2024-10-06 16:29:18 +0300

    Initial Start

---
+++ backend:/bus-reservation-application/src/test/java/com/waleed/bus_reservation_application/BusReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waleed.bus_reservation_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
